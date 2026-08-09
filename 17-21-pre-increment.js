58d75d9c9a327c23d939e1c9b5ac35c0e0abd3cd
Aminul islam <aminulislam424842@gmail.com>
2026-08-09 16:11:26 +0600

    feat: implement pre-increment function

---
+++ 17-21-pre-increment.js
@@ -0,0 +1,12 @@
+// let a=5;
+// console.log(++a)
+// console.log(a)
+
+const  preIncrement = (value=0)=> {
+  ++value;
+  return value;
+}
+
+// console.log(preIncrement(6));
+// console.log(preIncrement());
+// console.log(preIncrement(60));
